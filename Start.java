a2e16b50ce2571a1d5409c9553bfb8e242f1de12
Mungal Sudam <MUngalsudam6675@gmail.com>
2022-01-22 09:51:05 +0530

    adding welcome another msg

--- Start.java
+++ Start.java
@@ -3,6 +3,7 @@ package empwages;
 public class Start {
 	public static void main(String[] args) {
 		System.out.println("Welcome Employee");
+		System.out.println("Welcome to our programming");
 	}
 
 }
